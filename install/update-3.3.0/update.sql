e3d13bc6c8f027f09a261b7cae19924a611c8b67
steinm <steinm@577cc39b-cfd1-4aa2-8bc3-eda205cea05a>
2012-08-28 07:47:47 +0000

    - new version 3.3.7

--- install/update-3.3.0/update.sql
+++ install/update-3.3.0/update.sql
@@ -5,7 +5,7 @@ CREATE TABLE `tblUserPasswordRequest` (
   `date` datetime NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY  (`id`)
 ) DEFAULT CHARSET=utf8;
-UPDATE tblVersion set major=3, minor=3, subminor=0;
+UPDATE tblVersion set major=3, minor=3, subminor=7;
 ALTER TABLE tblDocumentContent MODIFY mimeType varchar(100);
 ALTER TABLE tblDocumentFiles MODIFY mimeType varchar(100);
 ALTER TABLE tblFolders ADD COLUMN `folderList` text NOT NULL;
