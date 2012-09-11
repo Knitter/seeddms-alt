24d41c956d34c0d8c2134449c2f50d8cbe58dd1b
steinm <steinm@577cc39b-cfd1-4aa2-8bc3-eda205cea05a>
2012-09-11 12:54:43 +0000

    - prevent xss attack

--- inc/inc.ClassUI.php
+++ inc/inc.ClassUI.php
@@ -693,7 +693,7 @@ class UI {
 			if ($folderID != $currentFolderID){
 			
 				if ($navigation) print "<a href=\"../out/out.ViewFolder.php?folderid=" . $folderID . "&showtree=1\">";
-				else print "<a class=\"foldertree_selectable\" href=\"javascript:folderSelected(" . $folderID . ", '" . str_replace("'", "\\'", $folder->getName()) . "')\">";
+				else print "<a class=\"foldertree_selectable\" href=\"javascript:folderSelected(" . $folderID . ", '" . str_replace("'", "\\'", htmlspecialchars($folder->getName())) . "')\">";
 
 			}else print "<span class=\"selectedfoldertree\">";
 			
