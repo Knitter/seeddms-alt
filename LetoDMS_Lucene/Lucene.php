d5b629769b56bd9554d1d0c6ecc7ac97980c46d3
steinm <steinm@577cc39b-cfd1-4aa2-8bc3-eda205cea05a>
2012-12-13 21:26:17 +0000

    update of copyright notice

--- LetoDMS_Lucene/Lucene.php
+++ LetoDMS_Lucene/Lucene.php
@@ -1,6 +1,6 @@
 <?php
 //    LetoDMS. Document Management System
-//    Copyright (C) 2011 Uwe Steinmann
+//    Copyright (C) 2011-2013 Uwe Steinmann
 //
 //    This program is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
