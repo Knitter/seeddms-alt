43eabc489e8b8d10c7bbd975b2d1ed8a9b5c0c44
steinm <steinm@577cc39b-cfd1-4aa2-8bc3-eda205cea05a>
2012-12-14 07:17:37 +0000

    - has been moved into install directory some time ago

--- UPDATE-v3.3.0/update.sql
+++
@@ -1,4 +0,0 @@
-UPDATE tblVersion set major=3, minor=3, subminor=0;
-ALTER TABLE tblDocumentContent MODIFY mimeType varchar(100);
-ALTER TABLE tblDocumentFiles MODIFY mimeType varchar(100);
-ALTER TABLE tblFolders ADD COLUMN `folderList` text NOT NULL;
