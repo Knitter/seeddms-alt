21437e5a90ff9fd8e07fc663be29ee9cb5b8cefa
Uwe Steinmann <steinm@debian.org>
2013-04-10 15:39:05 +0200

    move filterDocumentLinks() from inc.Util.php to Core/inc.ClassDMS.php

--- SeedDMS_Core/Core/inc.ClassDMS.php
+++ SeedDMS_Core/Core/inc.ClassDMS.php
@@ -206,6 +206,24 @@ class SeedDMS_Core_DMS {
 	} /* }}} */
 
 	/**
+	 * Filter document links
+	 *
+	 * Returns a filtered list of links which are accessible by the
+	 * given user.
+	 *
+	 * @param array $links list of objects of type SeedDMS_Core_DocumentLink
+	 * @param object $user user for which access is being checked
+	 * @return filtered list of links
+	 */
+	static function filterDocumentLinks($user, $links) { /* {{{ */
+		$tmp = array();
+		foreach ($links as $link)
+			if ($link->isPublic() || ($link->getUser()->getID() == $user->getID()) || $user->isAdmin())
+				array_push($tmp, $link);
+		return $tmp;
+	} /* }}} */
+
+	/**
 	 * Create a new instance of the dms
 	 *
 	 * @param object $db object to access the underlying database
