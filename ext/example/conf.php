e64ceabc7cf7dea38d9799cadbe06c984d4e9cd6
Uwe Steinmann <steinm@debian.org>
2013-04-18 22:18:46 +0200

    add more files to example extension

--- ext/example/conf.php
+++ ext/example/conf.php
@@ -1,11 +1,16 @@
 <?php
-$_GLOBALS['SEEDDMS_EXT']['example'] = array(
+$EXT_CONF['example'] = array(
 	'title' => 'Example Extension',
 	'description' => 'Example Extension',
 	'version' => '1.0.0',
 	'author' => array('name'=>'Uwe Steinmann', 'email'=>'uwe@steinmann.cx', 'company'=>'MMK GmbH'),
 	'constraints' => array(
 		'depends' => array('php' => '5.4.4-', 'seeddms' => '4.2.0-'),
+	),
+	'icon' => '',
+	'class' => array(
+		'file' => 'class.example.php',
+		'name' => 'ext_example'
 	),
 );
 ?>
