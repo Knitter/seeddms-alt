b091c7f861cef29170158c8b1d3b731487de0757
Uwe Steinmann <steinm@debian.org>
2013-05-02 22:33:59 +0200

    add sample configuration

--- ext/example/conf.php
+++ ext/example/conf.php
@@ -4,6 +4,17 @@ $EXT_CONF['example'] = array(
 	'description' => 'This sample extension demonstrate the use of various hooks',
 	'version' => '1.0.0',
 	'author' => array('name'=>'Uwe Steinmann', 'email'=>'uwe@steinmann.cx', 'company'=>'MMK GmbH'),
+	'config' => array(
+		'input_field' => array(
+			'title'=>'Example input field',
+			'type'=>'input',
+			'size'=>20,
+		),
+		'checkbox' => array(
+			'title'=>'Example check box',
+			'type'=>'checkbox',
+		),
+	),
 	'constraints' => array(
 		'depends' => array('php' => '5.4.4-', 'seeddms' => '4.2.0-'),
 	),
