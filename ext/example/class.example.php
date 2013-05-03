d11b2fa784cb3c7e009d2c305a52cd04ffb7e0d8
Uwe Steinmann <steinm@debian.org>
2013-05-03 13:24:27 +0200

    more documentation on which global variables can be used

--- ext/example/class.example.php
+++ ext/example/class.example.php
@@ -36,6 +36,14 @@ class SeedDMS_ExtExample extends SeedDMS_ExtBase {
 	 * Initialization
 	 *
 	 * Use this method to do some initialization like setting up the hooks
+	 * You have access to the following global variables:
+	 * $GLOBALS['dms'] : object representing dms
+	 * $GLOBALS['user'] : currently logged in user
+	 * $GLOBALS['session'] : current session
+	 * $GLOBALS['settings'] : current global configuration
+	 * $GLOBALS['settings']['_extensions']['example'] : configuration of this extension
+	 * $GLOBALS['LANG'] : the language array with translations for all languages
+	 * $GLOBALS['SEEDDMS_HOOKS'] : all hooks added so far
 	 */
 	function init() { /* {{{ */
 		$GLOBALS['SEEDDMS_HOOKS']['addDocument'][] = new SeedDMS_ExtExample_AddDocument;
