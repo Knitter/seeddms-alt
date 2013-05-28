add2aade8fd294ddd4af0a0e628559ded579be1e
Uwe Steinmann <steinm@debian.org>
2013-05-28 09:01:49 +0200

    add new field regex to tblAttributeDefinitions

--- install/update-4.3.0/update.sql
+++ install/update-4.3.0/update.sql
@@ -2,6 +2,8 @@ START TRANSACTION;
 
 ALTER TABLE tblSessions ADD COLUMN `splashmsg` TEXT DEFAULT '';
 
+ALTER TABLE tblAttributeDefinitions ADD COLUMN `regex` TEXT DEFAULT '';
+
 UPDATE tblVersion set major=4, minor=3, subminor=0;
 
 COMMIT;
