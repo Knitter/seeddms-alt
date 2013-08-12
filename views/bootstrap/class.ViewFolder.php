12f81b47ebe8cd1855a089b69029c6979b992920
Uwe Steinmann <steinm@debian.org>
2013-08-12 07:03:24 +0200

    call hook folderMenu

--- views/bootstrap/class.ViewFolder.php
+++ views/bootstrap/class.ViewFolder.php
@@ -90,7 +90,12 @@ class SeedDMS_View_ViewFolder extends SeedDMS_Bootstrap_Style {
 
 		$this->globalNavigation($folder);
 		$this->contentStart();
-		$this->pageNavigation($this->getFolderPathHTML($folder), "view_folder", $folder);
+		$txt = $this->callHook('folderMenu', $folder);
+		if(is_string($txt))
+			echo $txt;
+		else {
+			$this->pageNavigation($this->getFolderPathHTML($folder), "view_folder", $folder);
+		}
 
 		echo $this->callHook('preContent');
 
