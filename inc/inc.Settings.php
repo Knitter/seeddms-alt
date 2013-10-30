abed5d10237c395e96817cf9229efbfa0ab6f48c
Uwe Steinmann <steinm@debian.org>
2013-10-30 09:57:12 +0100

    if SEEDDMS_CONFIG_FILE is set, it will be passed to Settings()

--- inc/inc.Settings.php
+++ inc/inc.Settings.php
@@ -47,7 +47,10 @@ if (file_exists("../inc/inc.Settings.old.php")) {
 }
 
 require_once('inc.ClassSettings.php');
-$settings = new Settings();
+if(defined("SEEDDMS_CONFIG_FILE"))
+	$settings = new Settings(SEEDDMS_CONFIG_FILE);
+else
+	$settings = new Settings();
 if(!defined("SEEDDMS_INSTALL") && file_exists(dirname($settings->_configFilePath)."/ENABLE_INSTALL_TOOL")) {
 	die("SeedDMS won't run unless your remove the file ENABLE_INSTALL_TOOL from your configuration directory.");
 }
