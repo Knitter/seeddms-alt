01e067a2c4dbf0ebf26ae4982892465ab0f64336
Uwe Steinmann <steinm@debian.org>
2014-05-16 11:05:53 +0200

    use container-fluid class for top navigation (Bug #157)

--- views/bootstrap/class.Bootstrap.php
+++ views/bootstrap/class.Bootstrap.php
@@ -161,7 +161,7 @@ background-image: linear-gradient(to bottom, #882222, #111111);;
 	function globalBanner() { /* {{{ */
 		echo "<div class=\"navbar navbar-inverse navbar-fixed-top\">\n";
 		echo " <div class=\"navbar-inner\">\n";
-		echo "  <div class=\"container\">\n";
+		echo "  <div class=\"container-fluid\">\n";
 		echo "   <a class=\"brand\" href=\"../out/out.ViewFolder.php?folderid=".$this->params['rootfolderid']."\">".(strlen($this->params['sitename'])>0 ? $this->params['sitename'] : "SeedDMS")."</a>\n";
 		echo "  </div>\n";
 		echo " </div>\n";
@@ -196,7 +196,7 @@ background-image: linear-gradient(to bottom, #882222, #111111);;
 	function globalNavigation($folder=null) { /* {{{ */
 		echo "<div class=\"navbar navbar-inverse navbar-fixed-top\">\n";
 		echo " <div class=\"navbar-inner\">\n";
-		echo "  <div class=\"container\">\n";
+		echo "  <div class=\"container-fluid\">\n";
 		echo "   <a class=\"btn btn-navbar\" data-toggle=\"collapse\" data-target=\".nav-col1\">\n";
 		echo "     <span class=\"icon-bar\"></span>\n";
 		echo "     <span class=\"icon-bar\"></span>\n";
