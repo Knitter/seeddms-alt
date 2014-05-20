71fb34cbca8284767191080c0bf033536426e5e3
Uwe Steinmann <steinm@debian.org>
2014-05-20 06:43:44 +0200

    check quota

--- op/op.AddDocument.php
+++ op/op.AddDocument.php
@@ -49,6 +49,13 @@ if ($folder->getAccessMode($user) < M_READWRITE) {
 	UI::exitError(getMLText("folder_title", array("foldername" => $folder->getName())),getMLText("access_denied"));
 }
 
+if($settings->_quota > 0) {
+	$remain = checkQuota($user);
+	if ($remain < 0) {
+		UI::exitError(getMLText("folder_title", array("foldername" => htmlspecialchars($folder->getName()))),getMLText("quota_exceeded", array('bytes'=>SeedDMS_Core_File::format_filesize(abs($remain)))));
+	}
+}
+
 $comment  = $_POST["comment"];
 $version_comment = $_POST["version_comment"];
 if($version_comment == "" && isset($_POST["use_comment"]))
