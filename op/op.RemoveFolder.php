ad7a9aa5df521945c2db838c07c030f660c20295
Sérgio Lopes <knitter.is@gmail.com>
2014-07-17 21:39:03 +0100

    Fixes syntax error due to git conflit markers being present after incomplete merge.

--- op/op.RemoveFolder.php
+++ op/op.RemoveFolder.php
@@ -1,4 +1,5 @@
 <?php
+
 //    MyDMS. Document Management System
 //    Copyright (C) 2002-2005  Markus Westphal
 //    Copyright (C) 2006-2008 Malcolm Cowe
@@ -31,91 +32,62 @@ $tmp = explode('.', basename($_SERVER['SCRIPT_FILENAME']));
 $controller = Controller::factory($tmp[1]);
 
 /* Check if the form data comes for a trusted request */
-if(!checkFormKey('removefolder')) {
-	UI::exitError(getMLText("folder_title", array("foldername" => getMLText("invalid_request_token"))),getMLText("invalid_request_token"));
+if (!checkFormKey('removefolder')) {
+    UI::exitError(getMLText("folder_title", array("foldername" => getMLText("invalid_request_token"))), getMLText("invalid_request_token"));
 }
 
-if (!isset($_POST["folderid"]) || !is_numeric($_POST["folderid"]) || intval($_POST["folderid"])<1) {
-	UI::exitError(getMLText("folder_title", array("foldername" => getMLText("invalid_folder_id"))),getMLText("invalid_folder_id"));
+if (!isset($_POST["folderid"]) || !is_numeric($_POST["folderid"]) || intval($_POST["folderid"]) < 1) {
+    UI::exitError(getMLText("folder_title", array("foldername" => getMLText("invalid_folder_id"))), getMLText("invalid_folder_id"));
 }
 $folderid = $_POST["folderid"];
 $folder = $dms->getFolder($folderid);
 
 if (!is_object($folder)) {
-	UI::exitError(getMLText("folder_title", array("foldername" => getMLText("invalid_folder_id"))),getMLText("invalid_folder_id"));
+    UI::exitError(getMLText("folder_title", array("foldername" => getMLText("invalid_folder_id"))), getMLText("invalid_folder_id"));
 }
 
 if ($folderid == $settings->_rootFolderID || !$folder->getParent()) {
-	UI::exitError(getMLText("folder_title", array("foldername" => $folder->getName())),getMLText("cannot_rm_root"));
+    UI::exitError(getMLText("folder_title", array("foldername" => $folder->getName())), getMLText("cannot_rm_root"));
 }
 
 if ($folder->getAccessMode($user) < M_ALL) {
-	UI::exitError(getMLText("folder_title", array("foldername" => $folder->getName())),getMLText("access_denied"));
+    UI::exitError(getMLText("folder_title", array("foldername" => $folder->getName())), getMLText("access_denied"));
 }
 
 /* save this for notification later on */
-$nl =	$folder->getNotifyList();
-$parent=$folder->getParent();
+$nl = $folder->getNotifyList();
+$parent = $folder->getParent();
 $foldername = $folder->getName();
 
 $controller->setParam('folder', $folder);
 $controller->setParam('index', $index);
-if(!$controller->run()) {
-	UI::exitError(getMLText("folder_title", array("foldername" => getMLText("invalid_folder_id"))),getMLText("invalid_folder_id"));
+if (!$controller->run()) {
+    UI::exitError(getMLText("folder_title", array("foldername" => getMLText("invalid_folder_id"))), getMLText("invalid_folder_id"));
 }
-
-<<<<<<< HEAD
-if ($notifier) {
-	$subject = "folder_deleted_email_subject";
-	$message = "folder_deleted_email_body";
-	$params = array();
-	$params['name'] = $foldername;
-	$params['folder_path'] = $folder->getFolderPathPlain();
-	$params['username'] = $user->getFullName();
-	$params['sitename'] = $settings->_siteName;
-	$params['http_root'] = $settings->_httpRoot;
-	$notifier->toList($user, $nl["users"], $subject, $message, $params);
-	foreach ($nl["groups"] as $grp) {
-		$notifier->toGroup($user, $grp, $subject, $message, $params);
-=======
-$nl =	$folder->getNotifyList();
+$nl = $folder->getNotifyList();
 $foldername = $folder->getName();
 if ($folder->remove()) {
-	// Send notification to subscribers.
-	if ($notifier) {
-/*
-		$subject = "###SITENAME###: ".$folder->getName()." - ".getMLText("folder_deleted_email");
-		$message = getMLText("folder_deleted_email")."\r\n";
-		$message .= 
-			getMLText("name").": ".$folder->getName()."\r\n".
-			getMLText("folder").": ".$folder->getFolderPathPlain()."\r\n".
-			getMLText("comment").": ".$folder->getComment()."\r\n".
-			"URL: ###URL_PREFIX###out/out.ViewFolder.php?folderid=".$folder->getID()."\r\n";
+    if ($notifier) {
+        $subject = "folder_deleted_email_subject";
+        $message = "folder_deleted_email_body";
+        $params = array();
+        $params['name'] = $foldername;
+        $params['folder_path'] = $folder->getFolderPathPlain();
+        $params['username'] = $user->getFullName();
+        $params['sitename'] = $settings->_siteName;
+        $params['http_root'] = $settings->_httpRoot;
+        $notifier->toList($user, $nl["users"], $subject, $message, $params);
+        foreach ($nl["groups"] as $grp) {
+            $notifier->toGroup($user, $grp, $subject, $message, $params);
 
-		$notifier->toList($user, $folder->_notifyList["users"], $subject, $message);
-		foreach ($folder->_notifyList["groups"] as $grp) {
-			$notifier->toGroup($user, $grp, $subject, $message);
-		}
-*/
-		$subject = "folder_deleted_email_subject";
-		$message = "folder_deleted_email_body";
-		$params = array();
-		$params['name'] = $foldername;
-		$params['folder_path'] = $parent->getFolderPathPlain();
-		$params['username'] = $user->getFullName();
-		$params['sitename'] = $settings->_siteName;
-		$params['http_root'] = $settings->_httpRoot;
-		$params['url'] = "http".((isset($_SERVER['HTTPS']) && (strcmp($_SERVER['HTTPS'],'off')!=0)) ? "s" : "")."://".$_SERVER['HTTP_HOST'].$settings->_httpRoot."out/out.ViewFolder.php?folderid=".$parent->getID();
-		$notifier->toList($user, $nl["users"], $subject, $message, $params);
-		foreach ($nl["groups"] as $grp) {
-			$notifier->toGroup($user, $grp, $subject, $message, $params);
-		}
->>>>>>> seeddms-4.3.4
-	}
+            // Left after manually tying to fix a merge conflict with markers having been pushed to repo...
+            //$params['url'] = "http".((isset($_SERVER['HTTPS']) && (strcmp($_SERVER['HTTPS'],'off')!=0)) ? "s" : "")."://".$_SERVER['HTTP_HOST'].$settings->_httpRoot."out/out.ViewFolder.php?folderid=".$parent->getID();
+            //$notifier->toList($user, $nl["users"], $subject, $message, $params);
+        }
+    }
 }
 
 add_log_line();
 
-header("Location:../out/out.ViewFolder.php?folderid=".$parent->getID()."&showtree=".$_POST["showtree"]);
-
+header("Location:../out/out.ViewFolder.php?folderid=" . $parent->getID() . "&showtree=" . $_POST["showtree"]);
 ?>
