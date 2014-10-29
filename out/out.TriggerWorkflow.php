3cf78a5d15d347fd7e4eadba752a9d3f31ad91bc
Uwe Steinmann <steinm@debian.org>
2014-10-29 09:08:39 +0100

    instanciate access restrictions and pass it to view

--- out/out.TriggerWorkflow.php
+++ out/out.TriggerWorkflow.php
@@ -23,6 +23,7 @@ include("../inc/inc.Utils.php");
 include("../inc/inc.DBInit.php");
 include("../inc/inc.Language.php");
 include("../inc/inc.ClassUI.php");
+include("../inc/inc.ClassAccessOperation.php");
 include("../inc/inc.Authentication.php");
 
 if (!isset($_POST["documentid"]) || !is_numeric($_POST["documentid"]) || intval($_POST["documentid"])<1) {
@@ -57,9 +58,13 @@ if (!is_object($transition)) {
 
 $folder = $document->getFolder();
 
+/* Create object for checking access to certain operations */
+$accessop = new SeedDMS_AccessOperation($document, $user, $settings);
+
 $tmp = explode('.', basename($_SERVER['SCRIPT_FILENAME']));
 $view = UI::factory($theme, $tmp[1], array('dms'=>$dms, 'user'=>$user, 'folder'=>$folder, 'document'=>$document, 'version'=>$version, 'transition'=>$transition));
 if($view) {
+	$view->setParam('accessobject', $accessop);
 	$view->show();
 	exit;
 }
